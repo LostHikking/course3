8c9adc3fedf255e0493da7f405b3317c897725cf
Ivan Poletsky <wiperus55@gmail.com>
2019-02-24 21:14:43 +0600

    Initial commit

---
+++ src/main/java/ru/omsu/imit/course3/Course3.java
@@ -0,0 +1,13 @@
+package ru.omsu.imit.course3;
+
+/**
+ * Hello world!
+ *
+ */
+public class Course3
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
