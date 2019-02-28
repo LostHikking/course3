c2b99e1fb9a053b788367b2f39f28cdefb39fe63
wIPeRUS55 <wiperus55@gmail.com>
2019-02-28 19:59:21 +0600

    first version without junit

--- src/main/java/ru/omsu/imit/course3/Client.java
+++ src/main/java/ru/omsu/imit/course3/Client.java
@@ -1,37 +1,26 @@
 package ru.omsu.imit.course3;
 
 import com.google.gson.Gson;
-import java.io.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Client {
     public static void main(String[] args) {
-
-    Person person = new Person("Vasya", "Pupkin", 15);
-    Gson gson = new Gson();
-    String gsoText = gson.toJson(person);
-    System.out.println(gsoText);
-    File dir1 = new File("C://SomeDir");
-    File file1 = new File("C://SomeDir//Hello.txt");
-    boolean created = dir1.mkdir();
-    if (created)
-        System.out.println("Папка создана");
-    try {
-        boolean createdfile = file1.createNewFile();
+        final List<Person> list = new ArrayList<>();
+        Gson gson = new Gson();
+        Server server = new Server();
+        Person person1 = new Person("T", "Kek", 27);
+        String request = gson.toJson(person1);
+        String resultJson = server.get(request);
+        Person personResult = gson.fromJson(resultJson, Person.class);
+        System.out.println(personResult);
+        server.add(request);
+        resultJson = server.get(request);
+        personResult = gson.fromJson(resultJson, Person.class);
+        System.out.println(personResult);
+        server.printAll();
     }
-            catch(IOException ex){
 
-            System.out.println(ex.getMessage());
-        }
-        try(FileWriter writer = new FileWriter(file1, false))
-        {
-
-            writer.write(gsoText);
-            writer.append('\n');
-            writer.write(gsoText);
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-        }
-    }
 }
